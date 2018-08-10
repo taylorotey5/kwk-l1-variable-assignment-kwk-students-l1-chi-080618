43c1fb0f0967718f7777b88712508af7186a59d4
Taylor Otey <taylorotey5@gmail.com>
2018-08-10 15:42:12 +0000

    automatically backed up by learn

---
+++ car.rb
@@ -0,0 +1,36 @@
+#OBJECT ORIENTIED PROGRAMMING notes 
+
+Class caller
+  def initialize = (model, make, year)
+    @carmodel = model
+    @carmake = make
+    @caryear = year
+  
+end 
+
+  def model 
+    @carmodel
+  end
+
+  def make
+    @carmake
+  end
+
+  def year
+    @caryear
+  end
+
+end
+
+toyota = Car.new("camry", "toyota", "2016")
+toyota.model
+toyota.make
+toyota.year
+
+puts "This is my #{toyota.model}. It is a #{toyota.make}. It was made in #{toyota.year}."
+puts toyota.model
+
+
+
+
+
